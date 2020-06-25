5fa64ae0570f7231278285f966fdf1ca6e232461
keegers <42276368+keegers@users.noreply.github.com>
2020-06-25 15:50:36 -0700

    reviewer and actor routes tests

---
+++ __tests__/reviewer.test.js
@@ -0,0 +1,40 @@
+const request = require('supertest');
+const app = require('../lib/app.js');
+const Reviewer = require('../lib/models/Reviewer.js');
+const { prepare } = require('../data-base-helpers/data-base-helpers');
+
+describe('tests reviewer routes', () => {
+  it('can create a reviewer with POST', async() => {
+    return await request(app)
+      .post('/api/v1/reviewers')
+      .send({ name: 'Alison Bechdel', company: 'imdb' })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.anything(),
+          name: 'Alison Bechdel',
+          company: 'imdb',
+          __v: 0
+        });
+      });
+  });
+
+  it('can get all reviewers with GET', async() => {
+    const reviewers = prepare(await Reviewer.find());  
+    
+    return await request(app)
+      .get('/api/v1/reviewers')
+      .then(res => {
+        expect(res.body).toEqual(reviewers);
+      });
+  });
+
+  it('can get reviewer by GET id', async() => {
+    const reviewer = prepare(await Reviewer.findOne());
+
+    return await request(app)
+      .get(`/api/v1/reviewers/${reviewer._id}`)
+      .then(res => {
+        expect(res.body).toEqual(reviewer);
+      });
+  });
+});
--- data-base-helpers/seed.js
+++ data-base-helpers/seed.js
@@ -1,7 +1,9 @@
 const chance = require('chance').Chance();
 const Studio = require('../lib/models/Studio');
+const Actor = require('../lib/models/Actor');
+const Reviewer = require('../lib/models/Reviewer');
 
-module.exports = async({ studios = 20 } = {}) => {
+module.exports = async({ studios = 20, actors = 50, reviewers = 25 } = {}) => {
 
   await Studio.create([...Array(studios)].map(() => ({
     name: chance.company(),
@@ -10,5 +12,16 @@ module.exports = async({ studios = 20 } = {}) => {
       state: chance.state(),
       country: chance.country()
     }
+  })));
+
+  await Actor.create([...Array(actors)].map(() => ({
+    name: chance.name(),
+    dob: chance.date(),
+    pob: chance.city()
+  })));
+
+  await Reviewer.create([...Array(reviewers)].map(() => ({
+    name: chance.name(),
+    company: chance.company()
   })));
 };
