4b5fa4a812782a153bf5ed69e2d9b12e99b7b003
keegers <42276368+keegers@users.noreply.github.com>
2020-06-29 12:07:10 -0700

    add reviewer delete route and check

--- __tests__/reviewer.test.js
+++ __tests__/reviewer.test.js
@@ -2,6 +2,7 @@ const request = require('supertest');
 const app = require('../lib/app.js');
 const Reviewer = require('../lib/models/Reviewer.js');
 const { prepare } = require('../data-base-helpers/data-base-helpers');
+const Review = require('../lib/models/Review.js');
 
 describe('tests reviewer routes', () => {
   it('can create a reviewer with POST', async() => {
@@ -41,4 +42,41 @@ describe('tests reviewer routes', () => {
         expect(res.body).toEqual(reviewer);
       });
   });
+
+  // it('can PATCH a reviewer by id', async() => {
+  //   const reviewer = prepare(await Reviewer.findOne());
+
+  //   return await request(app)
+  //     .patch(`/api/v1/reviewers/${reviewer._id}`)
+  //     .send({
+  //       name: { }
+  //     })
+  //     .then(res => {
+  //       expect(res.body).toEqual(reviewer);
+  //     });
+    
+  // });
+
+  it('can delete a reviewer by id if there are no reviews', async() => {
+    const reviewer = prepare(await Reviewer.create({ name: 'new persons', company: 'imdb' }));
+    
+    return await request(app)
+      .delete(`/api/v1/reviewers/${reviewer._id}`)
+      .then(res => {
+        expect(res.body).toEqual(reviewer);
+      });
+
+  });
+
+  it('cannot delete a reviewer with reviews', async() => {
+    const reviewer = prepare(await Reviewer.findOne());
+    return await request(app)
+      .delete(`/api/v1/reviewers/${reviewer._id}`)
+      .then(res => {
+        expect(res.body).toEqual({ 'message': 'There are reviews cannot delete reviewer',
+          'status': 500 });
+      });
+
+  });
+
 });
